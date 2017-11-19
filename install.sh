0545e91bdac0802cc4996cc93a232ea5ede5e0f9
Chris <chr33s@icloud.com>
2017-11-19 19:48:48 +0200

    config: eslintrc

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-files=( 'vim' 'vimrc' 'screenrc' 'bash' 'bashrc' 'bash_profile' 'local' 'gitconfig' 'config' 'curlrc' 'gnupg' 'ssh' 'editorconfig' )
+files=( 'vim' 'vimrc' 'screenrc' 'bash' 'bashrc' 'bash_profile' 'local' 'gitconfig' 'config' 'curlrc' 'gnupg' 'ssh' 'editorconfig' 'eslintrc' )
 
 for file in "${files[@]}"
 do
