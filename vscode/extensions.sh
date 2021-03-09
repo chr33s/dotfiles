96ac5cd783b8203a46c58644408bc4647c66d3c9
chris <chr33s@icloud.com>
2021-03-09 21:54:41 +0700

    add: vscode remote extensions

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -5,5 +5,7 @@ sudo -v
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension EditorConfig.EditorConfig
 code --install-extension esbenp.prettier-vscode
-code --install-extension stylelint.vscode-stylelint
 code --install-extension github.github-vscode-theme
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension stylelint.vscode-stylelint
