bd213942949f05b14e2b661ebec883401374d739
chris <chr33s@icloud.com>
2021-08-16 09:20:38 +0700

    update: config for react-native m1

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -9,6 +9,7 @@ export GOPATH=${HOME}/go
 export HOMEBREW_NO_ANALYTICS=1
 export LSCOLORS=ExGxBxDxCxEgEdxbxgxcxd
 export PATH=${HOME}/.local/bin:$PATH:./bin:./.bin:./node_modules/.bin:${GOPATH}/bin
+export PATH=$PATH:$ANDROID_HOME/emulator:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools
 export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 export TERM=xterm-256color
 export ZSH_DOTENV_PROMPT=false
