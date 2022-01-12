5d9c7063969f1e5c4e170ada75e783b388d4e366
chris <chr33s@icloud.com>
2022-01-12 15:18:23 +0700

    update: vscode extensions

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -2,12 +2,19 @@
 
 sudo -v
 
+code --install-extension aaron-bond.better-comments
+code --install-extension bierner.comment-tagged-templates
+code --install-extension bradlc.vscode-tailwindcss
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension EditorConfig.EditorConfig
 code --install-extension esbenp.prettier-vscode
 code --install-extension GitHub.github-vscode-theme
+code --install-extension golang.go
+code --install-extension GraphQL.vscode-graphql
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-vscode-remote.remote-containers
 code --install-extension ms-vscode-remote.remote-ssh
 code --install-extension ms-vscode-remote.remote-ssh-edit
+code --install-extension Shopify.theme-check-vscode
+code --install-extension silvenon.mdx
 code --install-extension stylelint.vscode-stylelint
