0cd1b7fefe77f71b5b8313f17c43f80eb28deb39
chris <chr33s@icloud.com>
2023-07-06 14:56:51 +0700

    update: zsh

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -8,10 +8,9 @@ export HOMEBREW_NO_ANALYTICS=1
 export LANG="en_GB.UTF-8"
 export LSCOLORS="ExGxBxDxCxEgEdxbxgxcxd"
 export PATH="${HOME}/.local/bin:${PATH}:./bin:./.bin:./node_modules/.bin:${GOPATH}/bin"
-export PATH="/opt/homebrew/bin:${PATH}"
+export PATH="${PATH}:/opt/homebrew/bin:"
 export PATH="${PATH}:${ANDROID_HOME}/emulator:${ANDROID_HOME}/tools:${ANDROID_HOME}/tools/bin:${ANDROID_HOME}/platform-tools"
 export PATH="${PATH}:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 export TERM=xterm-256color
-export ZSH_DOTENV_PROMPT=false
 
 . $(brew --prefix asdf)/libexec/asdf.sh
--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -4,18 +4,15 @@ if [ ! -d $ZINIT_HOME ]; then
 fi
 source "${ZINIT_HOME}/zinit.zsh"
 
-zinit load "zsh-users/zsh-syntax-highlighting"
-zinit load "zsh-users/zsh-autosuggestions"
-zinit load "zsh-users/zsh-history-substring-search"
-if zinit loaded "zsh-users/zsh-history-substring-search" >> /dev/null; then
-  bindkey '^[[A' history-substring-search-up
-  bindkey '^[[B' history-substring-search-down
-fi
-zinit ice from"github"
-zinit load "mafredri/zsh-async"
-zinit ice pick"pure.zsh" from"github" as"theme"
-zinit load "sindresorhus/pure"
-if zinit loaded "sindresorhus/pure" >> /dev/null; then
-  zstyle :prompt:pure:git:fetch only_upstream yes
-  zstyle :prompt:pure:git:stash show yes
-fi
+zinit ice from"github" depth"1"
+zinit ice compile'(pure|async).zsh' pick'async.zsh' src'pure.zsh'
+zinit light sindresorhus/pure
+zstyle :prompt:pure:git:fetch only_upstream yes
+zstyle :prompt:pure:git:stash show yes
+zinit light "zsh-hooks/zsh-hooks"
+zinit light "zdharma-continuum/fast-syntax-highlighting"
+zinit light zsh-users/zsh-completions
+zinit light "zsh-users/zsh-autosuggestions"
+zinit light "zsh-users/zsh-history-substring-search"
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
