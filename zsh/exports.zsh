f997ed16d3767d728c8856f4d534e915ddaee389
chris <chr33s@icloud.com>
2025-03-31 16:27:52 +0700

    update: $PATH
    Signed-off-by: chris <chr33s@icloud.com>

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -7,9 +7,10 @@ export GOPATH=${HOME}/go
 export GPG_TTY=$(tty)
 export HOMEBREW_NO_ANALYTICS=1
 export LANG="en_GB.UTF-8"
-export PATH="${HOME}/.local/bin:${PATH}:./bin:./.bin:./node_modules/.bin:${GOPATH}/bin"
-export PATH="${PATH}:/opt/homebrew/bin:"
+export PATH="${PATH}:/opt/homebrew/bin"
 export PATH="${PATH}:${ANDROID_HOME}/emulator:${ANDROID_HOME}/tools:${ANDROID_HOME}/tools/bin:${ANDROID_HOME}/platform-tools"
+export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
+export PATH="${PATH}:${HOME}/.local/bin:./bin:./node_modules/.bin:${GOPATH}/bin"
 export PATH="${PATH}:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 export PATH="${PATH}:/Users/chr33s/.lmstudio/bin"
 
